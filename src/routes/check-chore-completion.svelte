906ac482a3ca1691985f6f8f4008715a218e03f3
Ruben Hensen <ruben.hensen@pm.me>
2021-10-12 09:35:05 +0200

    Temp bug fix absence page

--- src/routes/check-chore-completion.svelte
+++ src/routes/check-chore-completion.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import { currentMatching, names, chores, archiveWeeks, absence } from '../stores';
 	import YesNo from '../lib/YesNo.svelte';
-import { matchAndUpdate } from 'src/matching';
-import { goto } from '$app/navigation';
+	import { matchAndUpdate } from '../matching';
+	import { goto } from '$app/navigation';
 
-	
 	function click() {
 		// Check number of absences
 		let match = matchAndUpdate(true, currentMatching, archiveWeeks, chores, names, absence);
